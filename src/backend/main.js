5f38662260610810949769251f1f4d0b439d34cf
ctabin10 <ctabin10@gmail.com>
2025-12-20 14:39:42 -0500

    added bucket and html stuff

--- src/backend/main.js
+++ src/backend/main.js
@@ -2,8 +2,9 @@ import HTMLParser from "./HTMLParser";
 
 
 
-const TEST_URL = "http://ninjaflex.com/"
+const TEST_URL = "https://ihasabucket.com/";
+const TEST_URL_PATH = "../../tests/ihasabucket.htm";
 
 const parserTest = new HTMLParser();
-const doc = parserTest.setHTMLString(TEST_URL);
+const doc = parserTest.setHTMLString(TEST_URL_PATH);
 console.log(doc.title);
