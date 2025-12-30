776b0ea0c87baf02bc221bc3de26c5e0a049842f
ethan-broome <etbroome05@gmail.com>
2025-12-30 13:57:41 -0500

    Created server heading and parsed some data from html page

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -1,9 +1,20 @@
 import HTMLParser from "./HTMLParser.js";
 
-async function test() {
-    const parser = new HTMLParser();
-    await parser.fetchHTML("https://www.youtube.com/@sonic77852");
-    document.getElementById("output").textContent = parser.html;
-}
+const parser = new HTMLParser();
 
-test();
+document.getElementById("submit-button").addEventListener("click", async function() {
+    await parser.fetchHTML(document.getElementById("url-input").value);
+
+    //output to index.html
+    document.getElementById("url").innerHTML = "<b>URL: </b>" + parser.getURL();
+    document.getElementById("title").innerHTML = "<b>TITLE: </b>" + parser.getTitle();
+    document.getElementById("num-links").innerHTML = "<b>NUMBER OF LINKS: </b>" + parser.numLinks();
+    document.getElementById("num-images").innerHTML = "<b>NUMBER OF IMAGES: </b>" + parser.numImages();
+    document.getElementById("num-embeds").innerHTML = "<b>NUMBER OF EMBEDS: </b>" + parser.numEmbeds();
+    document.getElementById("compat-mode").innerHTML = "<b>COMPATABILITY MODE: </b>" + parser.compatMode();
+    document.getElementById("content-type").innerHTML = "<b>CONTENT TYPE: </b>" + parser.contentType();
+    document.getElementById("char-set").innerHTML = "<b>CHARACTER SET: </b>" + parser.characterSet();
+    document.getElementById("num-imported-fonts").innerHTML = "<b>NUMBER OF IMPORTED FONTS: </b>" + parser.numImportedFonts();
+    document.getElementById("character-counts").innerHTML = parser.printCharCount();
+    document.getElementById("html").textContent = parser.getHTML();
+});
