7b385ff5d73a6931346ada45215071fe8564f923
ethan-broome <etbroome05@gmail.com>
2026-01-14 10:22:15 -0500

    Implemented Seeding System

--- src/frontend/src/classes/SongBuilder.js
+++ src/frontend/src/classes/SongBuilder.js
@@ -7,12 +7,16 @@ import { initStrudel,
 
 class SongBuilder {
 
-    //local data
+    //strudel
+    isReady = false;
+
+    //patterns
     patterns = []
     song;
+
+    //song data
     tempo;
     beatsPerBar;
-    isReady = false;
 
     //html data
     docURL;
@@ -27,6 +31,7 @@ class SongBuilder {
     importedFonts;
 
     //seeds
+    OFFSET;
     DRUM_SEED;
     MELODY_SEED;
     CHORD_SEED;
@@ -34,7 +39,7 @@ class SongBuilder {
     KEY_SIGNATURE_SEED;
 
     //instrument arrays:
-    drumSounds = ["bd", "sd", "rim", "oh", "lt", "mt", "ht", "rd", "cr"];
+    drumSounds = ["~", "bd", "sd", "rim", "oh", "lt", "mt", "ht", "rd", "cr"];
 
 
 
@@ -43,7 +48,7 @@ class SongBuilder {
 
 
 
-    //constructors
+    //CONSTRUCTORS
     constructor() {
         this.initialize();
     }
@@ -55,7 +60,18 @@ class SongBuilder {
 
 
 
-    //get html data
+    //UTILITY
+    stringToAscii(word) {
+
+        let sum = 0;
+
+        for (let i = 0; i < word.length; i++) {
+            sum += word.charCodeAt(i);
+        }
+
+        return sum;
+    }
+
     getHTMLData(htmlData) {
         this.docURL = htmlData.url;        
         this.docTitle = htmlData.title;
@@ -70,37 +86,7 @@ class SongBuilder {
 
 
 
-    //set seeds
-    setSeeds() {
-
-        //Seed Formula: The number of occurrences in the html of a character decided by "SomeHtmlProperty % NumberOfUniqueCharacters"
-
-        const chars = Object.keys(this.charCounts);
-        const numChars = chars.length;
-        
-        const drumIndex = this.numLinks % numChars;
-        const melodyIndex = this.numImages % numChars;
-        const chordIndex = this.docURL.length % numChars;
-        const tempoIndex = this.docTitle.length % numChars;
-        const keyIndex = this.charSet.length % numChars;
-        
-        this.DRUM_SEED = this.charCounts[chars[drumIndex]];
-        this.MELODY_SEED = this.charCounts[chars[melodyIndex]];
-        this.CHORD_SEED = this.charCounts[chars[chordIndex]];
-        this.TEMPO_SEED = this.charCounts[chars[tempoIndex]];
-        this.KEY_SIGNATURE_SEED = this.charCounts[chars[keyIndex]];
-
-
-        console.log("Drum Seed: ", this.DRUM_SEED);
-        console.log("Melody Seed: ", this.MELODY_SEED);
-        console.log("Chord Seed: ", this.CHORD_SEED);
-        console.log("Tempo Seed: ", this.TEMPO_SEED);
-        console.log("Key Signature Seed: ", this.KEY_SIGNATURE_SEED);
-    }
-
-
-
-    //setters
+    //SETTERS
     setTempo() {
         this.tempo = this.TEMPO_SEED % 300;
     }
@@ -111,14 +97,60 @@ class SongBuilder {
 
 
 
-    //pattern handling
-    appendPattern(tempPattern) {
+    //SEED GENERATION
+    setSeeds() {
+
+        //Seed Formula: The number of occurrences in the html of a character decided by "SomeHtmlProperty % NumberOfUniqueCharacters"
+
+        const chars = Object.keys(this.charCounts);
+        const numChars = chars.length;
+
+        //mod to prevent overflow
+        this.OFFSET = this.stringToAscii(this.docURL) % 10000000;
+        
+        const drumIndex = this.numLinks % numChars;
+        const melodyIndex = this.numImages % numChars;
+        const chordIndex = this.docURL.length % numChars;
+        const tempoIndex = this.docTitle.length % numChars;
+        const keyIndex = this.charSet.length % numChars;
+        
+        const drumCount = this.charCounts[chars[drumIndex]];
+        const melodyCount = this.charCounts[chars[melodyIndex]];
+        const chordCount = this.charCounts[chars[chordIndex]];
+        const tempoCount = this.charCounts[chars[tempoIndex]];
+        const keySignatureCount = this.charCounts[chars[keyIndex]];
+                
+        this.DRUM_SEED = drumCount * (this.OFFSET);
+        this.MELODY_SEED = melodyCount * (this.OFFSET);
+        this.CHORD_SEED = chordCount * (this.OFFSET);
+        this.TEMPO_SEED = tempoCount * (this.OFFSET);
+        this.KEY_SIGNATURE_SEED = keySignatureCount * (this.OFFSET);
+
+        console.log("Offset: ", this.OFFSET);
+        console.log("Drum Seed: ", this.DRUM_SEED);
+        console.log("Melody Seed: ", this.MELODY_SEED);
+        console.log("Chord Seed: ", this.CHORD_SEED);
+        console.log("Tempo Seed: ", this.TEMPO_SEED);
+        console.log("Key Signature Seed: ", this.KEY_SIGNATURE_SEED);
+    }
+
+
+
+    //PATTERN GENERATION
+    generateDrums() {
+
+    }
+
+
+
+    //PATTERN HANDLING
+    appendPattern(newPattern) {
         console.log("appendPattern called");
 
-        if (tempPattern === null)
+        if (newPattern === null)
             return;
 
-        this.patterns.push(tempPattern);
+        this.patterns.push(newPattern);
     }
 
     buildPatterns() {
@@ -143,7 +175,7 @@ class SongBuilder {
 
 
     
-    //start and stop
+    //START AND STOP
     play() {
 
         if (this.isReady === false) {
